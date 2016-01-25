d5f7530fc567fce4ede1b7be555d25f610657ced
Altieres de Matos <altieres.matos@matera.com>
2016-01-25 19:16:31 -0200

    Dashboard

--- web/app/js/scripts.js
+++ web/app/js/scripts.js
@@ -1,5 +1,12 @@
 angular.module("ondetemjogo", []);
 
-angular.module("ondetemjogo").controller("loginController", function () {
+angular.module("ondetemjogo").controller("loginController", function ($scope) {
+	$scope.efetuarLogin = function (login) {
+		console.log($scope.login);
+	}
+
+});
+
+angular.module("ondetemjogo").controller("dashboardController", function ($scope) {
 
 });
