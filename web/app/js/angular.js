c54dce6a0e3b0777db2d4fc984ab923843a193ab
Arthur Fritz Santiago <arthur.santiago@matera.com>
2016-01-27 08:02:59 -0200

    Atualização da tela index

--- web/app/js/angular.js
+++ web/app/js/angular.js
@@ -12,5 +12,13 @@ angular.module("ondetemjogo").controller("dashboardController", function ($scope
 });
 
 angular.module("ondetemjogo").controller("welcomeController", function ($scope) {
-
+	$(function() {
+		$('a.scroll').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href').replace("#/index","")).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
 });
